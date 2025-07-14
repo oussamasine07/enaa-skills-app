7fb65cc12c7e4e3c76af72a977a8b1902ae05acc
youssef eddib <eddiby79@gmail.com>
2025-07-14 17:29:32 +0200

    creation testing fonctien

---
+++ briefing-service/src/main/java/com/enaaskills/briefingservice/mapper/BriefMapper.java
@@ -0,0 +1,4 @@
+package com.enaaskills.briefingservice.mapper;
+
+public class BriefMapper {
+}
--- briefing-service/src/test/java/com/enaaskills/briefingservice/service/BriefServiceTest.java
+++ briefing-service/src/test/java/com/enaaskills/briefingservice/service/BriefServiceTest.java
@@ -1,15 +1,37 @@
 package com.enaaskills.briefingservice.service;
 
+
+import com.enaaskills.briefingservice.model.Brief;
+import com.enaaskills.briefingservice.model.Engagement;
+import com.enaaskills.briefingservice.service.BriefService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+@ActiveProfiles("test")
 @SpringBootTest
-class BriefServiceTest {
-@Autowired
-private BriefService briefService;
+public class BriefServiceTest {
+
+    @Autowired
+    private BriefService briefService;
+
     @Test
-    void addBrief() {
+    void testAddBrief() {
+        Brief brief = new Brief();
+        brief.setTitle("Test brief");
+        brief.setDescription("Test description");
+        brief.setLaunchDate(LocalDate.now());
+        brief.setStartDate(LocalDate.of(2025, 7, 10));
+        brief.setEndDate(LocalDate.of(2025, 7, 20));
+        brief.setDuration(10);
+        brief.setEngagement(Engagement.INDIVIDUAL);
+
+        var response = briefService.addBrief(brief);
+
+        Assertions.assertNotNull(response.getBody());
     }
 }
