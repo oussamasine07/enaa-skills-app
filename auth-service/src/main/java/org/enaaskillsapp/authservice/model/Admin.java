b1b716150fcf5f844a8dc3a750fd762eb09a8490
oussamasine07 <sineoussama@gmail.com>
2025-07-16 12:38:13 +0100

    make needed models

---
+++ auth-service/src/main/java/org/enaaskillsapp/authservice/model/Admin.java
@@ -0,0 +1,8 @@
+package org.enaaskillsapp.authservice.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Admin extends User{
+
+}
---
+++ auth-service/src/main/java/org/enaaskillsapp/authservice/model/UserPrinciple.java
@@ -0,0 +1,51 @@
+package org.enaaskillsapp.authservice.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrinciple implements UserDetails {
+
+    private final User user;
+
+    public UserPrinciple (User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
