965cfd43f79113847335424c1babf4dba55db47d
oussamasine07 <sineoussama@gmail.com>
2025-07-23 14:48:31 +0100

    create new api gateway

--- validation-service/src/main/java/com/enaaskills/validationservice/ValidationServiceApplication.java
+++ validation-service/src/main/java/com/enaaskills/validationservice/ValidationServiceApplication.java
@@ -2,10 +2,12 @@ package com.enaaskills.validationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ValidationServiceApplication {
 
     public static void main(String[] args) {
